19f5614ebe56d12543ff0b98ce680069ff1f06ba
KCFlynn <kellyflynn3@msn.com>
2018-05-28 12:47:55 -0700

    another attempt

--- Scripts/resetGame.js
+++ Scripts/resetGame.js
@@ -1,11 +1,11 @@
 // a function that resets the variables and re-starts the game
 // resets the messages displayed in the Html page with the initial messages
 
-require(["main", "generateRandomNumber"],(function (Main, GenerateRandomNumber) {
+define(function () {
 
     return {
         resetGame: function() {
-    randomNumber = GenerateRandomNumber.generateRandomNumber();
+    randomNumber = generateRandomNumber();
     //resets the number of tries 
     numberOfTries = 0;
     //resets the number of tries left to the maximum number of tries allowed
@@ -22,8 +22,15 @@ require(["main", "generateRandomNumber"],(function (Main, GenerateRandomNumber) 
     $('#divFireworks').css("display", "none");   // >> 3
     //document.getElementById("messageParagr").innerText = startGameMsg;
     $('#messageParagr').text(startGameMsg)  //>> 7
-    Main.enableButtons();
+    enableButtons();
         }
     } 
-})
-);
+});
+
+/*in restGame.js  you are
+require(["main", "generateRandomNumber"],(function (Main, GenerateRandomNumber) {
+define(function () {
+
+2 bad things.  (1) you are saying this code needs "main".  It does not, its the other way around.
+(2) you don't want this code to run as soon as the browser downloads it, so require is not the correct verb.  It needs to be a define(function () {
+look at the project we did.*/
